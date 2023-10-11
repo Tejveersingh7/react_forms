335f3956be03cb065ac25d29270a4c175dc6822c
Tejveersingh7 <73236941+Tejveersingh7@users.noreply.github.com>
2023-10-11 12:18:18 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    const [fullName, setFullName] = useState({fName:"", lName:""});
+    // const [finalName, setFinalName] = useState({finalFName:"", finalLName:""})
+
+
+    // const Clicked = (events) => {
+    //     events.preventDefault(events);
+    //      setFinalName({finalFName:fullName.fName, finalLName:fullName.lName})
+    //     //  alert('form submitted')
+        
+    // }
+
+
+    const onChange = (events) => {
+        const value = events.target.value
+        const name = events.target.name
+        setFullName((preValue) =>{
+            if(name==='fName'){
+
+                return{
+                    fname:value,
+                    lName:preValue.lName
+                    
+                }}else if(name==='lName'){
+                    return{
+                        fname:preValue.fName,
+                        lName:value
+                    }
+                }
+        } 
+       )
+
+       console.log(fullName.fName)
+       console.log(fullName.lName)
+        
+    }
+
+
+    return (
+        <>
+            <h1>{fullName.fName} {fullName.lName}</h1>
+            <input type="text" onChange={onChange}  name = "fName" placeholder="Enter your first name  "/>
+            <input type="text" onChange={onChange}  name = "lName" placeholder="Enter your last name  "/>
+            <button >Submit</button>
+
+        </>
+    )
 }
 
 export default App;
